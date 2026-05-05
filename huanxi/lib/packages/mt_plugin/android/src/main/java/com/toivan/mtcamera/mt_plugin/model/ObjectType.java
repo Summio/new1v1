cd2acc94714c430b67282131774512d139e8c942
Huanxi Dev <dev@huanxi.com>
2026-05-05 21:16:44 +0800

    chore: 删除美颜模块和 mt_plugin 包
    移除以下内容：
    - beauty 模块 (beauty_camera_view, beauty_panel, beauty_controller, beauty_settings_page)
    - mt_plugin 包 (含 Android/iOS 原生代码和 FaceBeauty SDK)
    - beauty 相关测试文件
    - 美颜图标资源

--- huanxi/lib/packages/mt_plugin/android/src/main/java/com/toivan/mtcamera/mt_plugin/model/ObjectType.java
+++
@@ -1,5 +0,0 @@
-package com.toivan.mtcamera.mt_plugin.model;
-
-public enum ObjectType {
-    Null, Boolean, Number, String, Map, Array
-}
--- huanxi/lib/packages/mt_plugin/ios/Classes/MtPlugin.h
+++
@@ -1,6 +0,0 @@
-#import <Flutter/Flutter.h>
-#import <UIKit/UIKit.h>
-
-@interface MtPlugin : NSObject<FlutterPlugin>
-
-@end
